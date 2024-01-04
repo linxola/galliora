a573375aeacb205e38b3561b6fa3a0ae514c47ea
Mykola Litynskyi <94116748+linxola@users.noreply.github.com>
2024-01-05 01:02:30 +0200

    Added a possibility for users to sign in using not only their email, but also their username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:username) }
 
-    it { is_expected.to validate_uniqueness_of(:username) }
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
 
     # Testing format's validation of username field
     it { is_expected.to allow_value('_User.Name-99_').for(:username) }
@@ -16,5 +16,27 @@ RSpec.describe User do
 
     it { is_expected.to validate_length_of(:name).is_at_most(64).allow_nil }
     it { is_expected.to validate_length_of(:about).is_at_most(256).allow_nil }
+  end
+
+  describe 'Class methods' do
+    describe '#find_for_database_authentication' do
+      let(:searchable_user) { create(:user, username: 'abc', email: 'findme@email.com') }
+      let(:other_user) { create(:user, username: 'qwerty', email: 'avoidme@email.com') }
+
+      before do
+        searchable_user
+        other_user
+      end
+
+      it 'finds user with username as login condition' do
+        expect(described_class.find_for_database_authentication({ login: 'Abc ' }))
+          .to eq(searchable_user)
+      end
+
+      it 'finds user with email as login condition' do
+        expect(described_class.find_for_database_authentication({ login: 'Findme@email.com ' }))
+          .to eq(searchable_user)
+      end
+    end
   end
 end
