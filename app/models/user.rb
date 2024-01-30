1acdfa29fa374c01e628ef006d0960bcba22cfb4
Mykola Litynskyi <udevedu@skiff.com>
2024-01-30 20:30:01 +0200

    Fixed devise's error because of the missing field in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  sign_in_count          :integer          default(0), not null
 #  unconfirmed_email      :string
 #  username               :string           default(""), not null
 #  created_at             :datetime         not null
--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_03_160000) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
