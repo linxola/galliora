08ea75ad1ffc1c952f61fb0dead1fac2b09ab58b
Mykola Litynskyi <94116748+linxola@users.noreply.github.com>
2024-03-16 01:03:26 +0200

    Changed confirmation controller with the sign-in screen for email confirmation notifications, refactored locales

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -13,15 +13,26 @@ module Users
     # end
 
     # GET /email_confirmation?confirmation_token=abcdef
-    # def show
-    #   super
-    # end
+    def show # rubocop:disable Metrics/AbcSize
+      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+      yield resource if block_given?
+
+      if resource.errors.empty?
+        set_flash_message!(:notice, :confirmed)
+      else
+        flash.alert = resource.errors.full_messages.last
+      end
+
+      respond_with_navigational(resource) do
+        redirect_to after_confirmation_path_for(resource_name, resource)
+      end
+    end
 
     protected
 
     # The path used after resending confirmation instructions.
     def after_resending_confirmation_instructions_path_for(_resource_name)
-      flash.notice = I18n.t('devise.confirmations.resend_instructions')
+      flash.notice = I18n.t('devise.confirmations.send_instructions')
       new_user_confirmation_path
     end
 
--- spec/requests/users/confirmations_spec.rb
+++ spec/requests/users/confirmations_spec.rb
@@ -4,19 +4,20 @@ require 'rails_helper'
 
 RSpec.describe 'Confirmations' do
   describe 'POST /email_confirmation' do
-    subject(:post_send_email) do
+    subject(:send_email) do
       post '/email_confirmation', params: user_params
     end
 
-    before { create(:user, email: 'sms@test.io', confirmed_at: nil) }
+    before do
+      create(:user, email: 'sms@test.io', confirmed_at: nil)
+      send_email
+    end
 
     context 'with valid parameters' do
       let(:user_params) { { user: { email: 'sms@test.io' } } }
 
-      before { post_send_email }
-
       it 'creates a flash message' do
-        expect(flash[:notice]).to eq(I18n.t('devise.confirmations.resend_instructions'))
+        expect(flash[:notice]).to eq(I18n.t('devise.confirmations.send_instructions'))
       end
 
       it 'redirects back to the same screen' do
@@ -31,14 +32,67 @@ RSpec.describe 'Confirmations' do
     context 'with invalid parameters' do
       let(:user_params) { { user: { email: '' } } }
 
-      before { post_send_email }
-
       it 'does not create a flash message' do
         expect(flash[:notice]).to be_nil
       end
 
       it 'returns http unprocessable_entity status' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'GET /email_confirmation?confirmation_token=abcdef' do
+    subject(:confirm_email) do
+      get '/email_confirmation', params: user_params
+    end
+
+    before { confirm_email }
+
+    context 'when user is unconfirmed' do
+      let(:user) { create(:user, email: 'sms@test.io', confirmed_at: nil) }
+      let(:user_params) { { confirmation_token: user.confirmation_token } }
+
+      it 'confirms the user' do
+        expect(user.reload.confirmed_at).not_to be_nil
+      end
+
+      it 'creates a flash message' do
+        expect(flash[:notice]).to eq(I18n.t('devise.confirmations.confirmed'))
+      end
+
+      it 'redirects to the sign in screen' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'returns http found status' do
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    context 'when user is already confirmed' do
+      let(:user) { create(:user, email: 'sms@test.io', confirmed_at:) }
+      let(:user_params) do
+        confirmation_token = Devise.friendly_token
+        user.update(confirmation_token:)
+        { confirmation_token: }
+      end
+      let(:confirmed_at) { Time.zone.local(2024, 2, 4, 20, 0) }
+
+      it 'confirms the user' do
+        expect(user.reload.confirmed_at).to eq(confirmed_at)
+      end
+
+      it 'creates a flash message' do
+        expect(flash[:alert]).to eq("Email #{I18n.t('errors.messages.already_confirmed')}")
+      end
+
+      it 'redirects to the sign in screen' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+
+      it 'returns http found status' do
+        expect(response).to have_http_status(:found)
       end
     end
   end
