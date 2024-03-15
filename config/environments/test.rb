56e041db717155d69448e8b8557dbf8c96a574b0
Mykola Litynskyi <94116748+linxola@users.noreply.github.com>
2024-03-16 00:59:57 +0200

    Set up email sending in development environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'localhost' }
+
+  config.action_mailer.default_url_options = { host: ENV.fetch('DOMAIN') }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
