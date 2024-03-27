ba906c801c55537c50bca6765f94d16c824d4d80
Mykola Litynskyi <udevedu@skiff.com>
2024-03-27 23:37:00 +0200

    WIP User pages creation and styling

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,15 +16,20 @@ module Users
     # end
 
     # DELETE /sign_out
-    # def destroy
-    #   super
-    # end
+    def destroy
+      super
+      flash.clear
+    end
 
     protected
 
     # If you have extra params to permit, append them to the sanitizer.
     def configure_sign_in_params
       devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password])
+    end
+
+    def after_sign_in_path_for(resource)
+      stored_location_for(resource) || user_path(current_user.id)
     end
 
     private
